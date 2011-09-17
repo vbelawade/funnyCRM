a9a64a0b4ea2fb40183de2bcf6d61a24f6bec850
Vishal Belawade <vishal.belawade@gmail.com>
2011-09-17 14:55:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunnyCRM::Application.config.secret_token = '2db120093499579ccbc0ca9655b36e9fc2d15dc6922facd1459305cf7bbd0e5a2106cb38c0d084e343544b0e9f8b5b3d2277adcea5d447bdb60e38de331bd74d'
